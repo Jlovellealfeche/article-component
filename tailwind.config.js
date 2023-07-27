8c0dccc44aa4dfe73d4713f8193e45ffd6a9a64e
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-07-27 16:16:25 +0200

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,23 @@
 module.exports = {
   content: [],
   theme: {
+    colors:{
+      current: "white",
+    },
     extend: {
       fontFamily: {
         primary:"Manrope",
       }
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+
+
   },
   plugins: [],
 }
